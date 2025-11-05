294c5132a57acb1c30d2041bc448f21efc866a89
malikendsley <malik.s.endsley@gmail.com>
2025-11-05 02:31:17 -0500

    all instructions except 0x8000 family (math) 0x0055 may have a bug

--- src/chip8.rs
+++ src/chip8.rs
@@ -4,6 +4,8 @@ use rand::prelude::*;
 pub static SCREEN_WIDTH: usize = 64;
 pub static SCREEN_HEIGHT: usize = 32;
 pub static SCREEN_AREA: usize = SCREEN_WIDTH * SCREEN_HEIGHT;
+pub static FONT_SIZE: usize = 5;
+pub static FONT_COUNT: usize = 16;
 
 // TODO: Simple tests
 // TODO: Depending on usage, this (these) may become *size
@@ -30,14 +32,16 @@ const fn idx_from_coord(x: usize, y: usize) -> usize {
 pub struct Chip8 {
     ram: [u8; 4096],
     v: [u8; 16],
-    i_reg: u16,
-    delay_timer: u16,
-    sound_timer: u16,
+    idx: u16,
+    delay_timer: u8,
+    sound_timer: u8,
     pc: u16,
     stack: chip8_stack::Chip8Stack,
     keypad: [bool; 16],
     display_buffer: [u8; SCREEN_AREA],
-    dirty: bool,
+    pub dirty: bool,
+    pub load_store_quirk: bool, // When true, increments idx on save and load
+    pub jp_v0_quirk: bool,      // When true, interprets BNNN as BXNN
 }
 
 impl Default for Chip8 {
@@ -45,7 +49,7 @@ impl Default for Chip8 {
         let mut chip_8 = Self {
             ram: [0; 4096],
             v: [0; 16],
-            i_reg: 0,
+            idx: 0,
             delay_timer: 0,
             sound_timer: 0,
             pc: 0x200,
@@ -53,6 +57,8 @@ impl Default for Chip8 {
             keypad: [false; 16],
             display_buffer: [0; 64 * 32],
             dirty: false,
+            load_store_quirk: false,
+            jp_v0_quirk: true,
         };
 
         const FONT_SET: [u8; 80] = [
@@ -95,22 +101,21 @@ impl Chip8 {
 
     fn decode_execute(&mut self, op: u16) {
         let nibble_1 = (op & 0xF000) >> 12;
-        // println!("Nibble 1: {:01X}", nibble_1);
         match nibble_1 {
-            0 => {
-                match op {
-                    0x00E0 => {
-                        self.display_buffer = [0; SCREEN_AREA];
-                        self.dirty = true;
-                    }
-                    0x00EE => self.pc = self.stack.pop(),
-                    _ => {
-                        // 0 ops besides 0x00E0 and 0x0EE are ignored on modern interpreters
-                    }
+            0 => match op {
+                0x00E0 => {
+                    self.display_buffer = [0; SCREEN_AREA];
+                    self.dirty = true;
                 }
-            }
+                0x00EE => self.pc = self.stack.pop(),
+
+                _ => {
+                    println!("SYS addr instruction, addr: {}", nnn(op));
+                }
+            },
             1 => self.pc = nnn(op),
 
+            // TODO: Add configurable quirk described in https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#bnnn-jump-with-offset
             2 => {
                 self.stack.push(self.pc);
                 self.pc = nnn(op);
@@ -126,14 +131,19 @@ impl Chip8 {
                 }
             }
             5 => {
-                if self.v[x(op)] == self.v[y(op)] {
-                    self.pc += 2;
+                if n(op) == 0x0000 {
+                    if self.v[x(op)] == self.v[y(op)] {
+                        self.pc += 2;
+                    }
+                } else {
+                    panic!("Unrecognized 5 instruction");
                 }
             }
             6 => self.v[x(op)] = nn(op),
 
             7 => self.v[x(op)] = self.v[x(op)].wrapping_add(nn(op)), // Cover overflow
 
+            // TODO: Math
             8 => {}
 
             9 => {
@@ -142,9 +152,14 @@ impl Chip8 {
                 }
             }
 
-            0xA => self.i_reg = nnn(op),
+            0xA => self.idx = nnn(op),
 
-            0xB => self.pc = nnn(op) + self.v[0] as u16,
+            0xB => {
+                self.pc = nnn(op) + self.v[0] as u16;
+                if self.jp_v0_quirk {
+                    self.pc += self.v[x(op)] as u16;
+                }
+            }
 
             0xC => self.v[x(op)] = ((rand::rng().next_u32() & 0xFFFF) as u8) & nn(op),
 
@@ -158,7 +173,7 @@ impl Chip8 {
                 self.v[0xF] = 0;
 
                 for row in 0..h {
-                    let b = self.ram[self.i_reg as usize + row];
+                    let b = self.ram[self.idx as usize + row];
                     for col in 0..w {
                         let bit = (b >> (7 - col)) & 1;
                         if bit == 0 {
@@ -192,6 +207,74 @@ impl Chip8 {
                 }
             },
 
+            0xF => match nn(op) {
+                0x0007 => self.v[x(op)] = self.delay_timer,
+                0x000A => {
+                    if let Some(k) = self.keypad.iter().position(|&p| p) {
+                        self.v[x(op)] = k as u8;
+                    } else {
+                        self.pc -= 2; // stall until a key is down
+                    }
+                }
+                0x0015 => self.delay_timer = self.v[x(op)],
+
+                0x0018 => self.sound_timer = self.v[x(op)],
+                0x001E => {
+                    let vx = self.v[x(op)] as u16;
+                    self.v[0xF] = (self.idx > 0x0FFF - vx) as u8;
+                    self.idx = self.idx.saturating_add(vx) & 0x0FFF;
+                }
+                0x0029 => self.idx = (FONT_SIZE as u16) * (self.v[x(op)] as u16),
+                0x0033 => {
+                    let vx = self.v[x(op)];
+                    self.ram[self.idx as usize] = vx / 100;
+                    self.ram[(self.idx + 1) as usize] = (vx / 10) % 10;
+                    self.ram[(self.idx + 2) as usize] = vx % 10;
+                }
+                // TODO: Does not pass Timendus, but possibly not an issue
+                0x0055 => {
+                    let i0 = self.idx as usize;
+                    println!("i0 is {}", i0);
+                    let num_v = x(op);
+                    println!(
+                        "Saving {} register{}",
+                        num_v + 1,
+                        if num_v == 0 { "" } else { "s" }
+                    );
+                    for i in 0..=num_v {
+                        println!("Saving register v{}", i);
+                        self.ram[i0 + i] = self.v[i];
+                        println!("RAM slot {} now contains {}", i0 + i, self.ram[i0 + i]);
+                    }
+                    if self.load_store_quirk {
+                        println!("Quirk");
+                        self.idx += (num_v as u16) + 1;
+                    }
+                }
+                0x0065 => {
+                    let i0 = self.idx as usize;
+                    println!("i0 is {}", i0);
+                    let num_v = x(op);
+                    println!(
+                        "Loading {} register{}",
+                        num_v + 1,
+                        if num_v == 0 { "" } else { "s" }
+                    );
+                    for i in 0..=num_v {
+                        println!("Loading register v{}", i);
+                        self.v[i] = self.ram[i0 + i];
+                        println!("Register {} now contains {}", i, self.v[i]);
+                    }
+                    if self.load_store_quirk {
+                        println!("Quirk");
+                        self.idx += (num_v as u16) + 1;
+                    }
+                }
+                _ => {
+                    panic!("Unsupported F-type instruction");
+                }
+            },
+
             _ => {
                 println!("Unrecognized instruction");
             }
@@ -204,14 +287,6 @@ impl Chip8 {
         self.pc += 2;
         // println!("Fetched {:04X}", op);
         self.decode_execute(op);
-    }
-
-    pub fn dirty(&self) -> bool {
-        self.dirty
-    }
-
-    pub fn clear_dirty(&mut self) {
-        self.dirty = false;
     }
 
     pub fn display_buffer(&self) -> &[u8] {
